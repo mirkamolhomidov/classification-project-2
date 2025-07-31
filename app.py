1c2f52cb0c251dd6ccc389f82434b26e179b1785
Web developer <150703003+mirkamolhomidov@users.noreply.github.com>
2025-07-31 13:28:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from fastai.vision.all import *
 import pathlib
 import io
+import platform
 
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
@@ -12,7 +13,7 @@ model = load_learner('classify-mohirdev.pkl')
 
 file = st.file_uploader('Rasm yuklash', type=['png', 'gif', 'jpg', 'jpeg'])
 if file:
-    st.image(file, caption='Yuklangan rasm', use_container_width=True)
+    st.image(io.BytesIO(img_bytes, caption='Yuklangan rasm', use_container_width=True)
     
     img_bytes = file.read()
     img = PILImage.create(io.BytesIO(img_bytes))
