2287247c5b985435c40a5b497ab7d13866b4377a
Web developer <150703003+mirkamolhomidov@users.noreply.github.com>
2025-07-31 13:38:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 import streamlit as st
-from fastai.vision.all import *
-import pathlib
+from fastai.vision.all import load_learner, PILImage
 import io
-import platform
 
-plt = platform.system()
-if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+st.title("Rasm Klassifikatsiyasi")
 
-st.title('Bu klassifikatsiya model')
+uploaded_file = st.file_uploader("Rasmni yuklang", type=['jpg', 'jpeg', 'png'])
 
-model = load_learner('classify-mohirdev.pkl')
-
-file = st.file_uploader('Rasm yuklash', type=['png', 'gif', 'jpg', 'jpeg'])
-if file:
-    img_bytes = file.read()
-    st.image(io.BytesIO(img_bytes), caption='Yuklangan rasm', use_container_width=True)
+if uploaded_file is not None:
+    img_bytes = uploaded_file.read()
     
     img = PILImage.create(io.BytesIO(img_bytes))
+    
+    st.image(img, caption="Yuklangan rasm", use_container_width=True)
+
+    model = load_learner("model.pkl")
 
     pred, pred_id, probs = model.predict(img)
 
-    st.success(f"Predict: {pred}")
-    st.info(f"Probablity: {probs[pred_id]:.3f}")
+    st.markdown(f"### ✅ Bashorat: **{pred}**")
+    st.markdown(f"### 📊 Ishonchlilik: **{probs[pred_id]:.2f}**")
